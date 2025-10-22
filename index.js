1adede87f0fc3497ad976fed781b5d4812958643
Trickster <Mehdi.Tarissi@emsi-edu.ma>
2025-10-22 16:14:33 +0100

    Guess the number game with attemps feature

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-// RANDOM NUMBER GENERATOR
+// Number Guessing Game
 
-const myButton = document.getElementById("myButton");
-const myLabel = document.getElementById("myLabel");
-const min=1;
-const max =6
-let randomNum;
+const minNum = 1;
+const maxNum = 100;
+const correctNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-myButton.onclick=function(){
-    randomNum= Math.floor(Math.random()*(max-min))+min;
-    myLabel.textContent=randomNum;
+let attemps = 0;
+let guess;
+let running = true;
+
+while (running) {
+    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}:`);
+    guess = Number(guess);
+    attemps++;
+
+    if(attemps  ==5) {
+        window.alert(`Sorry, you've exceeded the maximum number of attempts. The correct number was ${correctNum}.`);
+        break;
+    }
+
+    if (guess < minNum || guess > maxNum || isNaN(guess)) {
+        window.alert(`Please enter a number between ${minNum} and ${maxNum}.`);
+    }
+    else if (guess < correctNum) {
+        window.alert("Too low! Try again.");
+    }
+    else if (guess > correctNum) {
+        window.alert("Too high! Try again.");
+    }
+    else {
+        window.alert(`Congratulations! You guessed the correct number ${correctNum} in ${attemps} attempts.`);
+        running = false;
+    }
+
 }
+
